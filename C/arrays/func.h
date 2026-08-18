cc9cee6287f6b6b765a747571afd6d13d15f4245
66Dave <24101409@usc.edu.ph>
2026-08-19 00:18:40 +0800

    Unfinished log(n)

---
+++ C/arrays/func.h
@@ -0,0 +1,31 @@
+
+
+
+void InsertSort(int arr[],int num, int *size){
+    //O(log(n))
+
+    //find Pos
+    int pos = 0,left,right, mid= (*size)/2;
+
+    
+    //half the array to find pos 
+    //lets find mid
+    
+    
+    //already find pos
+    
+    printf("position is: %d", pos);
+
+
+}
+
+
+
+void print(int arr[], int size){
+    int i;
+    printf("\nArray: { ");
+    for(i = 0; i < size;i++){ 
+        printf("%d ",arr[i]);
+    }
+    printf(" }");
+}
---
+++ C/arrays/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "func.h"
+
+
+#define Max 10
+
+void main(){
+
+    int arr[Max] = { 10 , 20 ,30 ,40 , 50 , 60};
+    int size = 6;
+    int num = 25;
+    print(arr, size);
+    InsertSort(arr, 22, &size);
+
+}
+
